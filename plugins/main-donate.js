e0cb934741cf385515be0c08ef11fbaa15e93d79
ashwinas543 <92850331+ashwinas543@users.noreply.github.com>
2023-06-18 10:13:02 +0530

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -4,8 +4,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     let don = `
 ≡ *DONATION*
 ONLY WANT YOUR LOVE
-SUPPORT ME BY MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM`
-let img = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
+SUPPORT ME BY MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM https://instagram.com/_not.luffy?igshid=MzNlNGNkZWQ4Mg==`
+let img = 'https://i.imgur.com/DEgswSB.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
